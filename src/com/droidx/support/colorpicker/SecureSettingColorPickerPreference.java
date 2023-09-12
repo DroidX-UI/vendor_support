CardinalOS/vendor_extentions_framework

support: Import ColorPreference Settings from crdroid
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

src/com/fortune/support/colorpicker/SecureSettingColorPickerPreference.java
@@ -1,0 +1,26 @@
+package com.fortune.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.fortune.support.preferences.SecureSettingsStore;
+import com.fortune.support.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}

src/com/fortune/support/colorpicker/SystemSettingColorPickerPreference.java
@@ -1,0 +1,26 @@
+package com.fortune.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.fortune.support.preferences.SystemSettingsStore;
+import com.fortune.support.colorpicker.ColorPickerPreference;
+public class SystemSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}

DroidX-UI/vendor_support

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

src/com/droidx/support/colorpicker/SecureSettingColorPickerPreference.java
@@ -1,0 +1,26 @@
+package com.droidx.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.droidx.support.preferences.SecureSettingsStore;
+import com.droidx.support.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}

src/com/droidx/support/colorpicker/SystemSettingColorPickerPreference.java
@@ -1,0 +1,26 @@
+package com.droidx.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.droidx.support.preferences.SystemSettingsStore;
+import com.droidx.support.colorpicker.ColorPickerPreference;
+public class SystemSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}